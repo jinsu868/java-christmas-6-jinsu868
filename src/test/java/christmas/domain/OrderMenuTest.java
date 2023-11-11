2523ab4b907111657dbb3476e45cd6e78cec50fc
jinsu868 <mjinsu098@gmail.com>
2023-11-12 00:05:20 +0900

    test(order): 구매 금액 계산 로직 테스트 추가

--- src/test/java/christmas/domain/OrderMenuTest.java
+++ src/test/java/christmas/domain/OrderMenuTest.java
@@ -1,11 +1,11 @@
 package christmas.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import christmas.error.IllegalArgumentExceptionType;
+import christmas.constant.Separator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderMenuTest {
@@ -21,5 +21,13 @@ class OrderMenuTest {
     @ValueSource(strings = {"양송이수프", "타파스", "크리스마스파스타", "제로콜라"})
     void 있는_메뉴_생성_성공_테스트(String menu) {
         OrderMenu orderMenu = new OrderMenu(menu, 1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"양송이수프-2,12000", "타파스-4,22000", "시저셀러드-3,24000"})
+    void 주문_메뉴_금액_계산_로직_테스트(String input, int result) {
+        String[] menuAndQuantity = input.split(Separator.DASH.getDivision());
+        OrderMenu orderMenu = new OrderMenu(menuAndQuantity[0], Integer.parseInt(menuAndQuantity[1]));
+        Assertions.assertThat(orderMenu.calculateOrderAmount()).isEqualTo(result);
     }
 }
