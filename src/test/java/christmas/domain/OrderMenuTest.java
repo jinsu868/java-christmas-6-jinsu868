c620a9c4280e148d244b7a0293f5fa879e951109
jinsu868 <mjinsu098@gmail.com>
2023-11-13 20:01:50 +0900

    test(orderMenu): 주문 수량 0개 이하 실패 테스트 추가

--- src/test/java/christmas/domain/OrderMenuTest.java
+++ src/test/java/christmas/domain/OrderMenuTest.java
@@ -24,6 +24,14 @@ class OrderMenuTest {
     }
 
     @ParameterizedTest
+    @ValueSource(ints = {0, -1, -50})
+    void 주문_수량_0_이하_실패_테스트(int quantity) {
+        assertThatThrownBy(() -> new OrderMenu("양송이수프", quantity))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @ParameterizedTest
     @CsvSource({"양송이수프-2,12000", "타파스-4,22000", "시저셀러드-3,24000"})
     void 주문_메뉴_금액_계산_로직_테스트(String input, int result) {
         String[] menuAndQuantity = input.split(Separator.DASH.getDivision());
