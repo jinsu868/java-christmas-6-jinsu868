90df84bfbf95c6f80ef55a2a391fed6079bd089c
jinsu868 <mjinsu098@gmail.com>
2023-11-13 20:57:27 +0900

    test(discount): 특별 할인 적용, 미적용 테스트 및 구매 총액 미달 테스트 추가

--- src/test/java/christmas/domain/DiscountManagerTest.java
+++ src/test/java/christmas/domain/DiscountManagerTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -43,4 +44,44 @@ class DiscountManagerTest {
         Assertions.assertThat(isDDayDiscount).isFalse();
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 17, 24, 25, 31})
+    void 특별_할인_적용_테스트(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        OrderMenu orderMenu = new OrderMenu("양송이수프", 2);
+        List<OrderMenu> orderMenus = new ArrayList<>();
+        orderMenus.add(orderMenu);
+        Order order = new Order(visitDate, orderMenus);
+        DiscountManager discountManager = new DiscountManager(order);
+        Map<DiscountType, Integer> discountResults = discountManager.getDiscountResults();
+        Assertions.assertThat(discountResults.get(DiscountType.SPECIAL)).isEqualTo(1000);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 5, 9, 11})
+    void 특별_할인_적용X_테스트(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        OrderMenu orderMenu = new OrderMenu("양송이수프", 2);
+        List<OrderMenu> orderMenus = new ArrayList<>();
+        orderMenus.add(orderMenu);
+        Order order = new Order(visitDate, orderMenus);
+        DiscountManager discountManager = new DiscountManager(order);
+        Map<DiscountType, Integer> discountResults = discountManager.getDiscountResults();
+        boolean isSpecialDiscount = discountResults.keySet().stream()
+                .anyMatch(discountType -> discountType.getType().equals(DiscountType.SPECIAL));
+        Assertions.assertThat(isSpecialDiscount).isFalse();
+    }
+
+    @Test
+    @DisplayName("구매 총액이 10000보다 작은 경우 할인이 적용되지 않는다.")
+    void 구매_총액_10000원_미만_할인_적용X_테스트() {
+        VisitDate visitDate = new VisitDate(1);
+        OrderMenu orderMenu = new OrderMenu("양송이수프", 1);
+        List<OrderMenu> orderMenus = new ArrayList<>();
+        orderMenus.add(orderMenu);
+        Order order = new Order(visitDate, orderMenus);
+        DiscountManager discountManager = new DiscountManager(order);
+        Map<DiscountType, Integer> discountResults = discountManager.getDiscountResults();
+        Assertions.assertThat(discountResults.size()).isEqualTo(0);
+    }
 }
