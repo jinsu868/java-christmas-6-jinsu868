f41bf4a6fff1dfb98bc1962ba403e5f919d872a7
jinsu868 <mjinsu098@gmail.com>
2023-11-14 20:37:01 +0900

    refactor(menu): 검증 책임 변경에 따른 테스트 코드 위치 변경

--- src/test/java/christmas/domain/OrderMenuTest.java
+++ src/test/java/christmas/domain/OrderMenuTest.java
@@ -11,19 +11,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 class OrderMenuTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"없는메뉴1", "없는메뉴2", "없는메뉴3", "없는메뉴4"})
-    void 없는_메뉴_생성_실패_테스트(String menu) {
-        assertThatThrownBy(() -> new OrderMenu(menu, 1))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"양송이수프", "타파스", "크리스마스파스타", "제로콜라"})
-    void 있는_메뉴_생성_성공_테스트(String menu) {
-        OrderMenu orderMenu = new OrderMenu(menu, 1);
-    }
-
-    @ParameterizedTest
     @ValueSource(ints = {0, -1, -50})
     void 주문_수량_0_이하_실패_테스트(int quantity) {
         assertThatThrownBy(() -> new OrderMenu("양송이수프", quantity))
